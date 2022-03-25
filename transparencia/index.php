616261c4ed5e8e84ac58eabd01feb14f3a652668
vyctor922 <victoralexandre922@gmail.com>
2022-03-25 17:25:37 -0500

    ajustes part 2

--- transparencia/index.php
+++ transparencia/index.php
@@ -6,13 +6,16 @@
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.css">
         <!-- Bootstrap icons CSS -->
         <link rel="stylesheet" href="./node_modules/bootstrap-icons/font/bootstrap-icons.css">
-
+        <!-- CSS personalizado -->
         <link rel="stylesheet" href="styles.css">
         <link rel="stylesheet" href="personalizado.css">
+        <!-- Ícone pagina -->
+        <link rel="shortcut icon" type="image/x-icon" href="https://sistemas.ufac.br/home/wp-content/themes/sistemas/staticIndex/imagens/logo_ufac.gif">
     </head>
 
     <body>
